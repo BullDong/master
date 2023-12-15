df3b6062d1759bfa300284db3758dd88c0548396
BullDong <760985261@qq.com>
2023-12-15 10:14:16 +0800

    修改
    修改

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = ""
+OPENAI_API_KEY = "sess-MLizbZAA7mAaBNbHdzsYHYoG61wHyZ4evvInuEgx"
 
 # openAi 官方 api
 URL = "https://api.openai.com/v1/chat/completions"
